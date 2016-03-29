5ece4947a711ac48c0917ced7d748aa8d6e73ffc
Kelyane <kelyane.lokita@gmail.com>
2016-03-29 15:12:29 +0000

    update

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 
@@ -9,7 +10,7 @@ if (!databaseUri) {
 
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://ds023088.mlab.com:23088/sakura',
-  cloud: process.env.CLOUD_CODE_MAIN  || __dirname + '/cloud/main.js',
+  cloud: process.env.CLOUD_CODE_MAIN  || __dirname + '/../client/main.js',
   appId: process.env.APP_ID || 'notepadId',
   masterKey: process.env.MASTER_KEY || '', 
   serverURL: process.env.SERVER_URL || 'https://notepad-mean-kelyane1.c9users.io/parse'
@@ -17,14 +18,21 @@ var api = new ParseServer({
 
 var app = express();
 
+app.use('/client', express.static(path.join(__dirname, '/../client')));
+
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('Raiz');
+  res.status(200).send('HIII!!!!');
 });
+
+app.get('/test', function(req, res) {
+  res.sendFile(path.join(__dirname, '/../client/index.html'));
+});
+
 
 var port = process.env.PORT;
 var httpServer = require('http').createServer(app);
